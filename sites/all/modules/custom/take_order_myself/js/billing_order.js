1bffae90c998c0bc41517debb1a31170065414fa
Denis <denis.buybarov@blinkreaction.com>
2014-02-06 18:57:59 +0200

    fix Take order setting on cart area.

--- sites/all/modules/custom/take_order_myself/js/billing_order.js
+++ sites/all/modules/custom/take_order_myself/js/billing_order.js
@@ -17,45 +17,63 @@ Drupal.theme.prototype.TOMModalFormPopup = function () {
   return html;
 }
 
+// Called after modal window has loaded.
 jQuery.fn.modal_window_position = function() {
+  modalWindowPosition(jQuery(this));
+}
+
+/**
+ * Positioned window.
+ * @param string id
+ *   element id
+ */
+function modalWindowPosition(id) {
   var topYloc = null;
   var scrollTop = jQuery(document).scrollTop();
   scrollTop = parseInt(scrollTop);
   var offset = topYloc + scrollTop + "px";
-  jQuery(this).animate({top:offset},{duration:700,queue:false});
+  jQuery(id).animate({top:offset},{duration:700,queue:false});
+}
+
+jQuery(document).ajaxComplete(function( event, xhr, settings ) {
+  if ( settings.url === "/system/ajax" ) {
+    availableAddresses();
+  }
+});
+
+/**
+ * Show / Hide available store addresses.
+ */
+function availableAddresses() {
+  var radio_tom = jQuery(".commerce_shipping input:radio[value=get_order_myself]");
+  if (radio_tom.prop("checked")) {
+    jQuery("#edit-myself-fildset").slideDown("fast");
+    jQuery(".form-item-store-addresses input:radio").prop("checked", false);
+  }
+  else {
+    jQuery("#edit-myself-fildset").slideUp("fast");
+  }
 }
 
 jQuery(document).ready(function($) {
-  var topYloc = null;
+  availableAddresses();
   // center positioned maps.
   jQuery(window).scroll(function () {
     modalWindowPosition("#modalContent")
   });
 
-  function modalWindowPosition(id) {
-    var scrollTop = jQuery(document).scrollTop();
-    scrollTop = parseInt(scrollTop);
-    var offset = topYloc + scrollTop + "px";
-    jQuery(id).animate({top:offset},{duration:700,queue:false});
-  }
-
-  /*
-  $("#edit-myself-fildset input.form-checkbox").click(function() {
+  jQuery(".commerce_shipping input.form-radio").click(function() {
+    console.warn(jQuery(this));
     if ($(this).is(":checked")) {
-       var group = "input:checkbox[name='" + $(this).attr("name") + "']";
+
+      var group = "input:checkbox[name='" + $(this).attr("name") + "']";
       $(group).prop("checked", false);
       $(this).prop("checked", true);
     } else {
       $(this).prop("checked", false);
     }
   });
-  */
 
-  addAttrToStore();
-
-  jQuery('.close-popup').click(function() {
-    hideAllMaps();
-  });
   /*
   var billingRadio = $("input[type='radio'].take_order").eq(0);
   var deliveryRadio = $("input[type='radio'].take_order").eq(1);
@@ -87,16 +105,12 @@ function isCheckedCheckboxDo(checkBox) {
     $("#edit-store-adresses").toggle(true)
     $("#edit-store-adresses").animate({
       }, 3000, 'swing', setReadOnly(true, $("fieldset input.can-disable")));
-  } 
-  else {
-    hideAllMaps();
-    //removeHelping();
-  }  
+  }
 }
 
 function deliveryChecked(checkBox) {
   if(checkBox.is(":checked")) {
-    //removeHelping();
+
   }
 }
 
@@ -115,64 +129,11 @@ function setReadOnly(is_set, elements) {
 }
 
 function isCheckedRadioDo(radioBox) {
-  //hideAllMaps();
-  console.warn(radioBox);
-
   var address = $("input#edit-customer-profile-shipping-commerce-customer-address-und-0-thoroughfare");
   var city = $("input#edit-customer-profile-shipping-commerce-customer-address-und-0-locality");
   //add_helping_text(false);
   address.val(radioBox.data('address'));
   city.val(radioBox.data("city"));
-}
-
-function addAttrToStore() {
-  hideAllMaps();
-
-  jQuery('.not-link').click(function() {
-    show_popup();
-    var clickedElem = jQuery(this).data("store-id");
-    jQuery('#view_store_loc_nid_' + clickedElem).css({'position': 'relative'}).animate({
-    }, 500, function() {
-      jQuery('#view_store_loc_nid_' + clickedElem).css({'position': 'relative', 'visibility': 'visible'});
-    });
-  });
-}
-
-function hideAllMaps() {
-  hide_popup();
-  jQuery('.view_store_loc').css({'position': 'absolute', 'visibility': 'hidden'});
-}
-
-function show_popup(){
-  var bodyH = jQuery(document).height();
-  jQuery('#fone-popup-block').css({
-    'width': '100%',
-    'height': bodyH + 'px',
-    'opacity': '0.8'
-  }).fadeIn("slow");
-}
-
-function hide_popup() {
-  jQuery('#fone-popup-block').fadeOut("slow");
-}
-
-function add_helping_text(is_add) {
-  if (is_add) {
-    var Text = Drupal.t("Please check available addresses");
-    $(".can-disable").after("<a class='helping_text' href='#edit-myself-fildset'>" + Text + "</a>");
-  }
-  else {
-    $('.helping_text').remove();
-  }
-}
-
-function removeHelping() {
-  //add_helping_text(false);
-  $("span.checked").removeClass("checked");
-  $("#edit-store-adresses").toggle(false);
-  $("#edit-store-adresses").removeClass("active").addClass("not-active");
-  $(".addresses.form-radio").attr("disabled", "disabled").removeAttr("checked");
-  setReadOnly(false, $("fieldset input.can-disable"));
 }
 
 });
